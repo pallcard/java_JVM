36cffec4717f37d6881b754b845d41f8b570b754
liuke <18162327131@163.com>
2021-02-01 12:29:43 +0800

    运行时数据区

--- main/main.go
+++ main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"jvmgo/classfile"
 	"jvmgo/classpath"
-	"strings"
+	"jvmgo/rtda"
 )
 
 func main() {
@@ -20,12 +20,51 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
-	fmt.Printf("classpath:%v class:%v args:%v\n", cp, cmd.class, cmd.args)
-	className := strings.Replace(cmd.class, ".", "/", -1)
-	cf := loadClass(className, cp)
-	fmt.Println(cmd.class)
-	printClassInfo(cf)
+	frame := rtda.NewFrame(100, 100)
+	testLocalVars(frame.LocalVars())
+	println("-------------")
+	testOperandStack(frame.OperandStack())
+	//cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	//fmt.Printf("classpath:%v class:%v args:%v\n", cp, cmd.class, cmd.args)
+	//className := strings.Replace(cmd.class, ".", "/", -1)
+	//cf := loadClass(className, cp)
+	//fmt.Println(cmd.class)
+	//printClassInfo(cf)
+}
+
+func testLocalVars(vars rtda.LocalVars) {
+	vars.SetInt(0, 100)
+	vars.SetInt(1, -100)
+	vars.SetLong(2, 2997924580)
+	vars.SetLong(4, -2997924580)
+	vars.SetFloat(6, 3.1415962)
+	vars.SetDouble(7, 2.231231212312312)
+	vars.SetRef(9, nil)
+	println(vars.GetInt(0))
+	println(vars.GetInt(1))
+	println(vars.GetLong(2))
+	println(vars.GetLong(4))
+	println(vars.GetFloat(6))
+	println(vars.GetDouble(7))
+	println(vars.GetRef(9))
+}
+
+func testOperandStack(ops *rtda.OperandStack) {
+	ops.PushInt(100)
+	ops.PushInt(-100)
+	ops.PushLong(2997924580)
+	ops.PushLong(-2997924580)
+	ops.PushFloat(3.1415962)
+	ops.PushDouble(2.231231212312312)
+	ops.PushRef(nil)
+	println(ops.PopRef())
+	println(ops.PopDouble())
+	println(ops.PopFloat())
+	println(ops.PopLong())
+	println(ops.PopLong())
+	println(ops.PopInt())
+	println(ops.PopInt())
+
 }
 
 func loadClass(className string, cp *classpath.ClassPath) *classfile.ClassFile {
